910a03fd4ad6ac2691b1a4fa7e7938cf499301c1
R4N63R <as1884as@gmail.com>
2022-08-26 11:51:21 +0300

    Create flag1.ps1

---
+++ flag1.ps1
@@ -0,0 +1,16 @@
+﻿function Randomkey {
+    return -join ((65..90) + (97..122) | Get-Random -Count 5 | ForEach-Object {[char]$_})
+}
+
+
+$wd = Randomkey
+
+echo $wd
+
+cd $env:TEMP
+
+mkdir $wd
+
+cd $wd
+
+echo "Rangerden selam patlamaya devam zıpıttı çıktı zıpıttı çıktı" > flag.txt
