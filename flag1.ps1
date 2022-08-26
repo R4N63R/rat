5f8d8d0011dac1efa849b69653ef0dc3b8df2138
R4N63R <as1884as@gmail.com>
2022-08-26 11:55:26 +0300

    Update flag1.ps1

--- flag1.ps1
+++ flag1.ps1
@@ -2,6 +2,8 @@
     return -join ((65..90) + (97..122) | Get-Random -Count 5 | ForEach-Object {[char]$_})
 }
 
+param ($param1)
+$param1 
 
 $wd = Randomkey
 
@@ -14,3 +16,5 @@ mkdir $wd
 cd $wd
 
 echo "Rangerden selam patlamaya devam zıpıttı çıktı zıpıttı çıktı" > flag.txt
+
+Remove-Item -Path $param1
